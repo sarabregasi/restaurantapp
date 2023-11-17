f21ae493f43d48480f7dd3347b736c408ec9d4de
sarabregasi <sarabregasi19@yahoo.com>
2023-11-17 20:07:53 +0100

    commit-17/11

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
-
+import Delivery from '../img/delivery.png'
 const MainContainer = () => {
   return (
-    <div>MainContainer</div>
+    <div className='grid grid-cols-1 md:grid-cols-2 gap-2'>
+      <div className='py-2 flex-1 flex flex-col items-start md:items-center justify-center'>
+      <div className='flex items-center gap-2 justify-center'>
+        <p>Bike Delivery</p>
+        <div className='w-10 h-10 bg-white rounded-full overflow-hidden'>
+        <img src={Delivery} className='w-full h-full object-contain' alt='delivery' />
+        </div>
+      </div>
+      </div>
+      <div className='py-2 bg-blue-400 flex-1'></div>
+    </div>
   )
 }
 
